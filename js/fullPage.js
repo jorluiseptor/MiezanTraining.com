71f035317ee5716ef59dbd640f61e165a1d718d9
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-01-16 11:50:46 -0500

    Converted to Materialize framework.

---
+++ js/fullPage.js
@@ -0,0 +1,224 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+  <head>
+    <link rel="preconnect" href="//cdnjs.cloudflare.com" pr="1.0" />
+    <link rel="preconnect" href="//www.algolia.com" pr="0.85" />
+    <link rel="dns-prefetch" href="//static.getclicky.com" />
+    <link rel="dns-prefetch" href="//www.google-analytics.com" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>fullPagejs - cdnjs.com - The free and open source CDN for web related libraries to speed up your website!</title>
+    <meta property="og:title" content="fullPagejs - cdnjs.com - The free and open source CDN for web related libraries to speed up your website!" />
+    <meta property="og:image" content="/img/logotext.png" />
+    <meta property="og:description" content="The free and open source CDN for all web libraries. Speed up your websites and save bandwidth!" />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="The free and open source CDN for all web libraries. Speed up your websites and save bandwidth!">
+    <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="cdnjs">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/united/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/theme.css">
+    <style>.gitter-open-chat-button{background-color:#dd4814;bottom:36px}</style>
+    <link rel="dns-prefetch" href="//cdn.carbonads.com" />
+  </head>
+
+  <body>
+    <style>#forkongithub a{background:#dd4814;color:#fff;text-decoration:none;font-family:arial,sans-serif;text-align:center;font-weight:bold;padding:5px 40px;font-size:1rem;line-height:2rem;position:relative;transition:0.5s;}#forkongithub a:hover{background:orange;color:#fff;}#forkongithub a::before,#forkongithub a::after{content:"";width:100%;display:block;position:absolute;top:1px;left:0;height:1px;background:#fff;}#forkongithub a::after{bottom:1px;top:auto;}@media screen and (min-width:1333px){#forkongithub{position:absolute;display:block;top:0;left:0;width:200px;overflow:hidden;height:200px;z-index:9999;}#forkongithub a{width:200px;position:absolute;top:30px;left:-45px;transform:rotate(315deg);-webkit-transform:rotate(315deg);-ms-transform:rotate(315deg);-moz-transform:rotate(315deg);-o-transform:rotate(315deg);box-shadow:4px 4px 10px rgba(0,0,0,0.8);}}</style><span id="forkongithub"><a href="https://goo.gl/2oDsTg">Fork me on GitHub</a></span>
+    <div class="container ">
+      <div class="row">
+        <div class="col-md-12">
+          <ul class="menu">
+            <li class="logo"><a href="/">cdnjs</a></li>
+            <li><a href="https://goo.gl/0pRcOk">Request a lib</a></li>
+            <li><a href="https://gitter.im/cdnjs/cdnjs">Chat</a></li>
+            <li><a href="https://www.cloudflare.com/network-map">Network</a></li>
+            <li><a href="http://stats.pingdom.com/4jg86a2wqei0">Uptime</a></li>
+            <li><a href="https://github.com/cdnjs/cdnjs/issues">Issues</a></li>
+           <!-- <li><a href="https://github.com/cdnjs/cdnjs/wiki/Extensions%2C-Plugins%2C-Resources">Plugins</a></li>-->
+            <li class=""><a href="/git_stats/general.html" title="~2015">git_stats</a></li>
+            <li class=""><a href="/gitstats/" title="~2015">gitstats</a></li>
+            <li class=""><a href="/about" class="translateit">About</a></li>
+            <li class=""><a href="/libraries" class="translateit">Browse Libraries</a></li>
+            </ul>
+       </div>
+       </div>
+
+  </div>
+
+    <div class="page">
+      <div class="container">
+      <div class="row">
+
+<div class="library-container">
+  <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-8">
+
+    <h1 style="margin-top: 00px;">fullPage.js</h1>
+    <p><a href="http:&#x2F;&#x2F;alvarotrigo.com&#x2F;fullPage&#x2F;">http:&#x2F;&#x2F;alvarotrigo.com&#x2F;fullPage&#x2F;</a>
+      <span id="library-name" style="display: none;" hidden>fullPage.js</span>
+      <p>
+        <li class="label label-default label-highlight"><a href="https://api.cdnjs.com/libraries/fullPage.js">cdnjs api</a> <a href="https://api.cdnjs.com/libraries/fullPage.js?output=human"><i class="fa fa-user"></i></a></li>
+        <a href="https:&#x2F;&#x2F;github.com&#x2F;alvarotrigo&#x2F;fullPage.js.git"><li class="label label-default label-highlight">git autoupdate enabled</li></a>
+        <li class="label label-default label-highlight"><a href="https:&#x2F;&#x2F;github.com&#x2F;alvarotrigo&#x2F;fullPage.js.git">repository</a></li>
+          <li class="label label-default label-highlight"><a href="https:&#x2F;&#x2F;spdx.org&#x2F;licenses&#x2F;MIT.html">MIT license</a></li>
+      </p>
+      <p>Create  beautiful fullscreen scrolling websites</p>
+      <em>jquery, scrolling, single_page, one_page, sliding</em>
+
+      <br />
+        </div>
+        <div class="col-md-4">
+
+      <script async type="text/javascript" src="https://cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=cdnjscom" id="_carbonads_js" async></script>
+        </div>
+      </div>
+       <div class="input-group version-dropdown">
+      <div class="input-group-addon">Version</div>
+      <select class="form-control version-selector" style="float: right;">
+          <option value="2.7.6" selected>2.7.6</option>
+          <option value="2.7.5" >2.7.5</option>
+          <option value="2.7.4" >2.7.4</option>
+          <option value="2.7.3" >2.7.3</option>
+          <option value="2.7.2" >2.7.2</option>
+          <option value="2.7.1" >2.7.1</option>
+          <option value="2.7.0" >2.7.0</option>
+          <option value="2.6.9" >2.6.9</option>
+          <option value="2.6.8" >2.6.8</option>
+          <option value="2.6.7" >2.6.7</option>
+          <option value="2.6.6" >2.6.6</option>
+          <option value="2.6.5" >2.6.5</option>
+          <option value="2.6.4" >2.6.4</option>
+          <option value="2.6.3" >2.6.3</option>
+          <option value="2.6.2" >2.6.2</option>
+          <option value="2.6.1" >2.6.1</option>
+          <option value="2.6.0" >2.6.0</option>
+          <option value="2.5.9" >2.5.9</option>
+          <option value="2.5.8" >2.5.8</option>
+          <option value="2.5.7" >2.5.7</option>
+          <option value="2.5.6" >2.5.6</option>
+          <option value="2.5.5" >2.5.5</option>
+          <option value="2.5.4" >2.5.4</option>
+          <option value="2.5.3" >2.5.3</option>
+          <option value="2.5.2" >2.5.2</option>
+          <option value="2.5.1" >2.5.1</option>
+          <option value="2.5.0" >2.5.0</option>
+          <option value="2.4.9" >2.4.9</option>
+          <option value="2.4.8.1" >2.4.8.1</option>
+          <option value="2.4.8" >2.4.8</option>
+          <option value="2.4.7" >2.4.7</option>
+          <option value="2.4.6" >2.4.6</option>
+          <option value="2.4.3" >2.4.3</option>
+          <option value="2.4.1" >2.4.1</option>
+          <option value="2.2.1" >2.2.1</option>
+          <option value="2.0.7" >2.0.7</option>
+          <option value="1.7.6" >1.7.6</option>
+          <option value="1.7.5" >1.7.5</option>
+          <option value="1.7" >1.7</option>
+      </select>
+        </div>
+      <div style="clear: both;"></div>
+      <br />
+      <div data-library-version="2.7.6" class="assets">
+          <table style="margin-top: 10px;" cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example">
+            <tbody>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="fullPage.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.6/jquery.fullPage.css</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="fullPage.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.6/jquery.fullPage.js</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="fullPage.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.6/jquery.fullPage.min.css</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="fullPage.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.6/jquery.fullPage.min.js</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="fullPage.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.6/vendors&#x2F;jquery.easings.min.js</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="fullPage.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.6/vendors&#x2F;jquery.slimscroll.js</p>
+                </td>
+              </tr>
+              <tr class="library">
+                <td class="library-column"  data-lib-name="fullPage.js" style="position: relative;">
+                  <p class='library-url'>https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.6/vendors&#x2F;jquery.slimscroll.min.js</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <h3>Related Tutorials</h3>
+        
+
+            <p style="margin-bottom: 50px;">Help us write awesome web development tutorials! <a href="https://github.com/cdnjs/tutorials" target="_blank">Submit your own community driven tutorials.</a></p>
+</div>
+</div>
+</div>
+
+
+    </div>
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">
+          Follow <a href="https://github.com/PeterDaveHello"><i class="fa fa-github"></i></a> <a href="https://twitter.com/intent/follow?region=follow_link&screen_name=peterdavehello&tw_p=followbutton"><i class="fa fa-twitter"></i></a> <a href="https://www.peterdavehello.org">@Peter Dave Hello</a> and <a href="https://twitter.com/intent/follow?region=follow_link&screen_name=cdnjs&tw_p=followbutton"><i class="fa fa-twitter"></i>@cdnjs</a> on GitHub and twitter to get latest news about cdnjs!
+        </p>
+      </div>
+    </footer>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0/handlebars.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap-dropdown.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap-tooltip.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.5/clipboard.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/algoliasearch/3.10.2/algoliasearch.min.js"></script>
+  <script src="/js/main.js"></script>
+
+    <script src="//static.getclicky.com/js"></script>
+    <script type="text/javascript">try{clicky.init(66376315);}catch(err){}</script>
+    <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66376315ns.gif" /></p></noscript>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-44291190-1', 'auto');
+      ga('require', 'linkid', 'linkid.js');
+      ga('require', 'displayfeatures');
+      ga('send', 'pageview');
+if($('.post').length > 0) {
+  $('.menu').parent().removeClass('col-md-12').addClass('col-md-10 col-md-offset-1');
+}
+
+$('.post > p, .post  > h1, .post > h2, .post > ul, .post > h3, .post > blockquote').wrap('<div class="container"><div class="row"><div class="col-md-10 col-md-offset-1">')
+$('.post > pre > code').wrapInner('<div class="container"><div class="row"><div class="col-md-10 col-md-offset-1">')
+$('.post code').addClass('hljs');
+$('.post').show();
+    </script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/highlight.min.js"></script>
+
+<script>hljs.initHighlightingOnLoad();</script>
+<script>
+  ((window.gitter = {}).chat = {}).options = {
+    room: 'cdnjs/cdnjs'
+  };
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gitter-sidecar/1.1.3/sidecar.js" async defer></script>
+</body>
+</html>
